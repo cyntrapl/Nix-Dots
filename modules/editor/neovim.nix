a30345872c269610bf0c17191a4df6d2d7b3c483
Cyntrap <cyntrap3@gmail.com>
2024-05-29 16:20:44 +0300

    Init

---
+++ modules/editor/neovim.nix
@@ -0,0 +1,85 @@
+{pkgs, lib, ...}:
+
+{
+  programs.neovim = 
+  let
+    toLua = str: "lua << EOF\n${str}\nEOF\n";
+    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
+  in
+  {
+    enable = true;
+    defaultEditor = true;
+
+
+    viAlias = true;
+    vimAlias = true;
+    vimdiffAlias = true;
+
+    extraPackages = with pkgs; [
+      lua-language-server
+      nil
+
+      xclip
+      wl-clipboard
+    ];
+
+    plugins = with pkgs.vimPlugins; [
+
+      {
+        plugin = nvim-lspconfig;
+        config = toLuaFile ./nvim/plugin/nil-lsp.lua;
+      }
+
+      {
+        plugin = comment-nvim;
+        config = toLua "require(\"Comment\").setup()";
+      }
+
+      {
+        plugin = tokyonight-nvim;
+        config = "colorscheme tokyonight";
+      }
+
+      neodev-nvim
+
+      nvim-cmp 
+      {
+        plugin = nvim-cmp;
+        config = toLuaFile ./nvim/plugin/cmp.lua;
+      }
+
+      {
+        plugin = telescope-nvim;
+        config = toLuaFile ./nvim/plugin/telescope.lua;
+      }
+
+      telescope-fzf-native-nvim
+
+      cmp_luasnip
+      cmp-nvim-lsp
+
+      luasnip
+      friendly-snippets
+
+
+      lualine-nvim
+      nvim-web-devicons
+
+      {
+        plugin = nvim-treesitter.withAllGrammars;
+	config = toLuaFile ./nvim/plugin/treesitter.lua;
+      }
+
+      vim-nix
+
+      # {
+      #   plugin = vimPlugins.own-onedark-nvim;
+      #   config = "colorscheme onedark";
+      # }
+    ];
+
+    extraLuaConfig = ''
+      ${builtins.readFile ./nvim/options.lua}
+    '';
+  };
+}
